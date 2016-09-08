8a3b8fa8699cb245d168db23b1b0382167a854d7
Gina <gdebell@gmail.com>
2016-09-08 17:04:57 -0600

    Get post created with title, genre, book_cover_url, and description is being added to the reads data table.  Will add author(s) at a later time. Next step......create a GET route to display all books on a html page

---
+++ src/server/db/migrations/20160908150956_create_reads.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('reads', function (table) {
+    table.increments();
+    table.string('title');
+    table.string('genre');
+    table.string('book_cover_url');
+    table.string('description');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('reads');
+};
--- src/server/routes/books.js
+++ src/server/routes/books.js
@@ -3,15 +3,29 @@ const router = express.Router();
 
 const indexController = require('../controllers/index');
 const genres = require('../genres');
+const knex = require('../db/knex');
 
-
+function Books() {return knex('reads'); }
 
 router.get('/', (req, res, next) => {
-  res.render('books/index', {title:'Books'});
+  res.render('books/index', {title: 'Books'});
 });
 
 router.get('/new', (req, res, next) => {
   res.render('books/new', {title: 'New Book', genres: genres});
+});
+
+router.post('/', (req, res, next) => {
+  let newBook = {
+    title: req.body.title,
+    genre: req.body.genre,
+    book_cover_url: req.body.book_cover_url,
+    description: req.body.description
+  };
+  new Books().insert(newBook)
+  .then(() => {
+    res.redirect('/books');
+  });
 });
 
 module.exports = router;
